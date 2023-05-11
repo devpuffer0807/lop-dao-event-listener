800a848af06bf482f1005b88fe83e93a6bb977e3
Puffer <pufferdev0807@gmail.com>
2023-05-11 15:44:40 +0900

    fixed vote no of shareholder

--- config/ShareHolderApi.js
+++ config/ShareHolderApi.js
@@ -44,8 +44,8 @@ const ShareHolderApi = {
   },
   VoteNo: async function (params) {
     try {
-      const proposalIndex = params[0];
-      const account = params[1];
+      const account = params[0];
+      const proposalIndex = params[1];
       const type = "SHAREHOLDER";
       const approve = "NO";
 
